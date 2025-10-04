37a307daac542f5c6d2690ac8bdc76f26345133e
Sandun Sanjeewa <sanjeewasandun82@gmail.com>
2025-10-04 12:50:44 +0530

    updated sidebar

--- client/src/containers/Sidebar.jsx
+++ client/src/containers/Sidebar.jsx
@@ -1,38 +1,41 @@
-import { Home } from "lucide-react";
+import { Home, Settings, LayoutDashboard, Building,MonitorSpeaker,MonitorCog,CalendarClock  } from "lucide-react";
 const Sidebar = () =>{
     return(
         <>
 
              <aside className="hidden md:flex fixed z-10 top-[60px] bottom-[20px] md:ml-2 
                      w-[40px] md:w-[50px] h-[calc(100vh-80px)] 
-                     flex-col bg-gray-900 divide-y divide-gray-700 rounded-md"
+                     flex-col bg-gray-900 divide-y divide-gray-700 rounded-md justify-around"
                     >
-                        <div>
+
+                        <div className="">
                             <div className="py-4 flex justify-center">
                                 <Home className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
                             </div>
                             <div className="py-4 flex justify-center">
-                                <Home className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
-                            </div>
-                            <div className="py-4 flex justify-center">
-                                <Home className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
+                                <LayoutDashboard className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
                             </div>
                         </div>
 
-                        <div>
+                        <div className=" ">
                             <div className="py-4 flex justify-center">
-                                <Home className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
+                                <Building className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
                             </div>
                             <div className="py-4 flex justify-center">
-                                <Home className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
+                                <MonitorSpeaker className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
                             </div>
                             <div className="py-4 flex justify-center">
-                                <Home className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
+                                <MonitorCog className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
+                            </div>
+                            <div className="py-4 flex justify-center">
+                                <CalendarClock className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2} />
                             </div>
                         </div>
 
-                        <div>
-                           
+                        <div className="">
+                           <div className="py-4 flex justify-center items-end">
+                            <Settings className="md:w-6 md:h-6 text-gray-100" strokeWidth={1.2}/>
+                           </div>
                         </div>
                     </aside>
 
